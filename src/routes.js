b86e5a13638135265ff5bb665959ec208bbf655f
Yago Siguinolfi <yagosiguinolfi@gmail.com>
2021-04-16 00:49:59 -0300

    sequelize configuration

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const UserController = require('./controllers/UserController');
+
+const routes = express.Router();
+/*
+routes.get('/', (req, res) => {
+  return res.json({ hello: 'Wolrd'});
+});*/
+
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
+module.exports = routes;
